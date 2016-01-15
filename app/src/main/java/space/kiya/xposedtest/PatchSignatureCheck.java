4578a6af32afbc110f819ad3c04df8cef9bfabc0
kiya-z <2267161320@qq.com>
2016-01-15 10:45:49 +0800

    update

--- app/src/main/java/space/kiya/xposedtest/PatchSignatureCheck.java
+++ app/src/main/java/space/kiya/xposedtest/PatchSignatureCheck.java
@@ -11,7 +11,7 @@ public class PatchSignatureCheck implements IXposedHookZygoteInit{
     @Override public void initZygote(StartupParam startupParam) throws Throwable {
         XposedHelpers.findAndHookMethod("java.security.Signature", null, "verify", byte[].class, new XC_MethodHook() {
             @Override protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("disabled verify signature.");
+                XposedBridge.log("disabled system verify signature.");
                 param.setResult(Boolean.TRUE);
             }
         });
